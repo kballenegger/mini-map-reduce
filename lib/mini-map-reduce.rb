c0d0c287215ef7614644b05034e0cb7a1b980496
Kenneth Ballenegger <kenneth@ballenegger.com>
2012-12-06 10:51:50 -0800

    initial blank commit

---
+++ lib/mini-map-reduce.rb
@@ -0,0 +1,9 @@
+require "mini-map-reduce/version"
+
+module Mini
+  module Map
+    module Reduce
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/mini-map-reduce/version.rb
@@ -0,0 +1,7 @@
+module Mini
+  module Map
+    module Reduce
+      VERSION = "0.0.1"
+    end
+  end
+end
